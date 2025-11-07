88603addba3dd264354b3755e93d63ddc3a55fae
Bejarano03 <marco.bejarano03@gmail.com>
2025-11-06 23:51:11 -0800

    had to troubleshoot prisma/client implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './user.service';
 
 @Module({
-  imports: [],
+  imports: [UserService],
   controllers: [AppController],
   providers: [AppService],
 })
